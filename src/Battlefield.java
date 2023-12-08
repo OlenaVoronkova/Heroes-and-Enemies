5b4c7dd7b84f35eadd9624560c0db2d89ca4f033
Олена Воронкова <145047410+OlenaVoronkova@users.noreply.github.com>
2023-12-08 17:40:54 +0200

    Fourth commit

--- src/Battlefield.java
+++ src/Battlefield.java
@@ -17,17 +17,17 @@ public class Battlefield {
 
         System.out.println("Finally, you're on the battlefield, my precious. Now, I choose the fitting hero for you, my precious.");
 
-        ArrayList<String> heroList = new ArrayList<>();
+        ArrayList<Hero> heroList = new ArrayList<>();
 
-        heroList.add("Legolas");
-        heroList.add("Gimli");
-        heroList.add("Aragorn");
+        heroList.add(new Hero("Legolas", "elf", 10, 20, 15, 100, 200, 50));
+        heroList.add(new Hero("Gimli", "dwarf", 20, 10, 15, 200, 150, 30));
+        heroList.add(new Hero("Aragorn", "man", 10, 15, 20, 100, 200, 40));
 
-        List<String> selectedHero = selectRandomHero(heroList, 1);
+        List<Hero> selectedHero = selectRandomHero(heroList, 1);
 
         System.out.println("Selected hero:");
-        for (String hero : selectedHero) {
-            System.out.println(hero);
+        for (Hero hero : selectedHero) {
+            System.out.println(hero.name);
 
         }
 
@@ -80,39 +80,41 @@ public class Battlefield {
         Enemy saruman = new Enemy("Saruman", "wizard", 100, 100, "КУСЬ", 30);
         Enemy shelob = new Enemy("Shelob", "spider", 100, 100, "КУСЬ", 30);
 
-        ArrayList<String> enemyList = new ArrayList<>();
+        ArrayList<Enemy> enemyList = new ArrayList<>();
 
-        enemyList.add("Gollum");
-        enemyList.add("Saruman");
-        enemyList.add("Shelob");
+        enemyList.add(new Enemy("Gollum", "unknown", 100, 100, "КУСЬ", 30));
+        enemyList.add(new Enemy("Saruman", "wizard", 100, 100, "КУСЬ", 30));
+        enemyList.add(new Enemy("Shelob", "spider", 100, 100, "КУСЬ", 30));
 
-        List<String> selectedEnemy = selectRandomEnemy(enemyList, 1);
+        List<Enemy> selectedEnemy = selectRandomEnemy(enemyList, 1);
         System.out.println("Selected enemy:");
-        for (String enemy : selectedEnemy) {
-            System.out.println(enemy);
-            System.out.println("Your enemy is " + enemy + ". Good luck!");
+        for (Enemy enemy : selectedEnemy) {
+            System.out.println(enemy.name);
+            System.out.println("Your enemy is " + enemy.name + ". Good luck!");
 
 
             while (aragorn.getHealthPoints() > 0 && shelob.getHP() > 0) {
-                List<String> heroName = selectedHero;
-                for (Skill skill : selectedSkills) {
-                    int heroDamage = aragorn.getDamage();
-                    System.out.println("Your hero is " + heroName + " his skill is " + skill.name + ", his damage is " + heroDamage + " .");
+                for ( Hero hero : selectedHero) {
+                    for (Skill skill : selectedSkills) {
+                        int heroDamage = aragorn.getDamage();
+                        System.out.println("Your hero is " + hero.name + " his skill is " + skill.name + ", his damage is " + heroDamage + " .");
+                    }
                 }
-            List<String> selectEnemy = selectedEnemy;
-            String enemySkill = shelob.skill;
-            int shelobDamage = shelob.getDamage();
-            System.out.println("Your enemy is " + selectEnemy + " her/his skill is " + enemySkill + " and her/his damage is " + shelobDamage + " . Good luck!");
 
-            int newHeroHPValue = aragorn.getHealthPoints() - shelobDamage;
-            int newEnemyHPValue = shelob.getHP() - aragorn.damage;
-            aragorn.setHealthPoints(newHeroHPValue);
-            shelob.setHP(newEnemyHPValue);
+           for (Enemy enemyName : selectedEnemy) {
+               String enemySkill = shelob.skill;
+               int shelobDamage = shelob.getDamage();
+               System.out.println("Your enemy is " + enemy.name + " her/his skill is " + enemySkill + " and her/his damage is " + shelobDamage + " . Good luck!");
+
+               int newHeroHPValue = aragorn.getHealthPoints() - shelobDamage;
+               int newEnemyHPValue = shelob.getHP() - aragorn.damage;
+               aragorn.setHealthPoints(newHeroHPValue);
+               shelob.setHP(newEnemyHPValue);
 
 
-            System.out.println("There is a new value of Aragorn hp: " + newHeroHPValue + " .");
-            System.out.println("There is a new value of Shelob hp: " + newEnemyHPValue + " .");
-
+               System.out.println("There is a new value of Aragorn hp: " + newHeroHPValue + " .");
+               System.out.println("There is a new value of Shelob hp: " + newEnemyHPValue + " .");
+           }
         }
 
 
@@ -135,13 +137,13 @@ public class Battlefield {
         return selectedSkills;
     }
 
-    public static List<String> selectRandomEnemy(List<String> enemysList, int count) {
-        List<String> selectedEnemy = new ArrayList<>();
+    public static List<Enemy> selectRandomEnemy(List<Enemy> enemysList, int count) {
+        List<Enemy> selectedEnemy = new ArrayList<>();
         Random rand = new Random();
 
         while (selectedEnemy.size() < count) {
             int randomIndex = rand.nextInt(enemysList.size());
-            String enemy = enemysList.get(randomIndex);
+            Enemy enemy = enemysList.get(randomIndex);
             if (!selectedEnemy.contains(enemy)) {
                 selectedEnemy.add(enemy);
             }
@@ -149,13 +151,13 @@ public class Battlefield {
 
         return selectedEnemy;
     }
-    public static List<String> selectRandomHero(List<String> heroList, int count) {
-        List<String> selectedHero = new ArrayList<>();
+    public static List<Hero> selectRandomHero(List<Hero> heroList, int count) {
+        List<Hero> selectedHero = new ArrayList<>();
         Random rand = new Random();
 
         while (selectedHero.size() < count) {
             int randomIndex = rand.nextInt(heroList.size());
-            String hero = heroList.get(randomIndex);
+            Hero hero = heroList.get(randomIndex);
             if (!selectedHero.contains(hero)) {
                 selectedHero.add(hero);
             }
